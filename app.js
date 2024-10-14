4e06da7c2d36aeed822eb0cc50e71477335e1636
Syed-Kumail-Naqvi <syedkumailnaqvi82@gmail.com>
2024-10-14 10:03:54 +0500

    Form-2

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
-var fullName = document. getElementById("fullName");
-var fatherName = document. getElementById("fatherName");
-var rollNumber = document. getElementById("rollNumber");
-var email = document. getElementById("email");
-var phoneNo = document. getElementById("phoneNo");
-var doBirth = document. getElementById("doBirth");
-var result = document. getElementById("output");
-var submit = document. getElementById("submit");
-var clear = document. getElementById("clear");
-var clearAll = document. getElementById("clearAll");
 
-function submitValue (){
+var forFullName = [];
+var forEmail = [];
+var forPhoneNumber = [];
+var forPassword = [];
+
+function takeInupts(){
+
+    var fullName = document.getElementById('fullName').value;
+
+    var YourEmail = document.getElementById('YourEmail').value;
+
+    var PhoneNumber = document.getElementById('PhoneNumber').value;
+
+    var YourPassword = document.getElementById('YourPassword').value;
+
+
+    forFullName.push(fullName);
+    forEmail.push(YourEmail);
+    forPhoneNumber.push(PhoneNumber);
+    forPassword.push(YourPassword);
+
+    document.getElementById('showText').innerHTML = `
+    <h2>Your Details Are Here</h2>
+    <p><b>Full Name:</b> ${forFullName}</p>
+    <p><b>Email :</b> ${forEmail}</p>
+    <p><b>Phone Number :</b>  ${forPhoneNumber}</p>
+    <p><b>Password :</b> ${forPassword}</p>
+    <p class='last'>Made By Syed Kumail Naqvi</p>`;
     
-    output.innerHTML = ["Full-Name: "] + fullName.value;
-    output2.innerHTML = ["Father's-Name: "] + fatherName.value; 
-    output3.innerHTML = ["Roll-Number: "] + rollNumber.value;
-    output4.innerHTML = ["Email: "] + email.value;
-    output5.innerHTML = ["Phone-No: "] + phoneNo.value;
-    output6.innerHTML = ["Date-of-Birth: "] + doBirth.value;      
+};
+
+function pop(){
+
+    forFullName.pop();
+    forEmail.pop();
+    forPhoneNumber.pop();
+    forPassword.pop();
+
+    document.getElementById('showText').innerHTML = `
+    <h2>Your Details is here</h2>
+    <p><b>Full Name:</b> ${forFullName}</p>
+    <p><b>Email :</b> ${forEmail}</p>
+    <p><b>Phone Number :</b>  ${forPhoneNumber}</p>
+    <p><b>Password :</b> ${forPassword}</p>
+    <p class='last'><strong>Design by Muhammad Waqas,  Roll No. 263696</strong></p>`;
+  
+};
+
+function shift(){
+
+    forFullName.shift();
+    forEmail.shift();
+    forPhoneNumber.shift();
+    forPassword.shift();
+
+    document.getElementById('showText').innerHTML = `
+    <h2>Your Information</h2>
+    <p><b>Full Name:</b> ${forFullName}</p>
+    <p><b>Email :</b> ${forEmail}</p>
+    <p><b>Phone Number :</b>  ${forPhoneNumber}</p>
+    <p><b>Password :</b> ${forPassword}</p>
+    <p class='last'>Made By Syed Kumail Naqvi</p>`;
+  
 }
-
-
-// submit.addEventListener('click', submitValue);
